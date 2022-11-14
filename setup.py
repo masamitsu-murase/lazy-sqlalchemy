8e21c9b69848392ede698a30680f21842bd9743d
Masamitsu MURASE <masamitsu.murase@gmail.com>
2022-11-15 00:56:47 +0900

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lazy-sqlalchemy',
+    version='1.0.0',
+    description='A library to wrap sqlalchemy for lazy load of database.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/masamitsu-murase/lazy-sqlalchemy',
+    author='Masamitsu MURASE',
+    author_email='masamitsu.murase@gmail.com',
+    license='MIT',
+    keywords='sqlalchemy',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=True,
+    python_requires='>=3.7.*, <4',
+    install_requires=['SQLAlchemy'],
+    extras_require={'test': [], 'package': ['wheel', 'twine']},
+    project_urls={
+        'Bug Reports':
+        'https://github.com/masamitsu-murase/lazy-sqlalchemy/issues',
+        'Source':
+        'https://github.com/masamitsu-murase/lazy-sqlalchemy',
+    },
+)
